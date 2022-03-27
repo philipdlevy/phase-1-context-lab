bceaf84ef4640191fccbedc01fef9a66d49e6daa
Philip Levy <philipdlevy@gmail.com>
2022-03-27 17:23:56 -0600

    commit tree 815e49b9fe5cca873e06ca40020b19dde56a309b

--- index.js
+++ index.js
@@ -1,5 +1,79 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+    return {
+        "firstName": array[0],
+        "familyName": array[1],
+        "title": array[2],
+        "payPerHour": array[3],
+        "timeInEvents": [],
+        "timeOutEvents": []
+
+    }
+}
+
+function createEmployeeRecords(arrays) {
+    // Converts each nested Array into an employee record using createEmployeeRecord and accumulates it to a new Array
+    let newArray = arrays.map(array => createEmployeeRecord(array))   
+    return newArray
+}
+
+function createTimeInEvent(dateStamp) {
+    // console.log("this", this)
+    let array = dateStamp.split(" ")
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(array[1], 10),
+        date: array[0]
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    
+    let splitDateStamp = dateStamp.split(" ")
+    
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(splitDateStamp[1], 10),
+        date: splitDateStamp[0]
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(formDate) {
+    // console.log("this", this)
+    // console.log("formDate", formDate)
+    
+    let timeIn = this.timeInEvents.find(element => {
+        // console.log("element", element)
+        return element.date === formDate
+        
+    })
+    // console.log("timeIn", timeIn)
+    
+    let timeOut = this.timeOutEvents.find(element => {
+        return element.date === formDate
+    })
+    // console.log("timeOut", timeOut)
+
+    let hoursWorked = (timeOut.hour - timeIn.hour) / 100
+
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(dateForm) {
+    // console.log("this", this)
+
+    const hoursWorked = hoursWorkedOnDate.call(this, dateForm) 
+    
+    const payOwed = hoursWorked * this.payPerHour
+
+    return payOwed
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +95,29 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName) {
+    // console.log("srcArray", srcArray)
+    // console.log("firstName", firstName)
+
+    let findName = srcArray.find(name => {
+        
+        return name.firstName === firstName
+        
+    }) 
+    return findName
+}
+
+function calculatePayroll(employeeArray) {
+    // console.log(employeeArray)
+    
+    let initialValue = 0
+
+    let payOwed = employeeArray.reduce((accumulator, currentEmployee) => {
+        // console.log("accumulator", accumulator)
+        return accumulator + allWagesFor.call(currentEmployee)
+    }, initialValue) 
+
+    // allWagesFor.call(this, employeeArray)
+
+    return payOwed
+}
